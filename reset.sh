f296058fab652728f3e7abed7bb159114dbc4a03
Eric Tsai <eric.tsai@cbinsights.com>
2019-05-01 16:15:45 -0400

    Created reset.sh script to reset develop and staging to latest master

---
+++ reset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "The absolute directory path must be passed in for the git repo you wish to reset"
+    exit 1
+fi
+
+echo " -- Navigating to " $1 " --"
+cd $1
+
+git fetch origin
+
+echo " -- Stashing any uncommitted changes --"
+git stash
+
+echo " -- Resetting 'develop' branch to latest master --"
+git checkout develop
+git reset --hard origin/master
+git push origin develop --no-verify --force
+
+echo " -- Resetting 'staging' branch to latest master --"
+git checkout staging
+git reset --hard origin/master
+git push origin staging --no-verify --force
+
+echo " -- Resetting 'master' branch to latest master --"
+git checkout master
+git reset --hard origin/master
+
+echo " -- Popping uncommitted changes --"
+git stash pop
