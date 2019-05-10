df91595b09ed5488057b3d3cb86e0d60aba78595
Eric Tsai <eric.tsai@cbinsights.com>
2019-05-10 16:55:15 -0400

    Added base timestamp so that we can calculate the end of the sprint correctly

--- reset.sh
+++ reset.sh
@@ -5,13 +5,26 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+if [ -z $2 ]; then
+    echo "A base timestamp (epoch) must be passed in to calculate the 2 week sprint cycle"
+    exit 1
+fi
+
+one_day_in_seconds=86400
+two_weeks_in_seconds=$(($one_day_in_seconds * 14))
+timestamp=$(date +%s)
+modulo=$((($timestamp - $2) % $two_weeks_in_seconds))
+abs_modulo=$(($modulo > 0 ? $modulo : ($modulo * -1)))
+
+if [ "$abs_modulo" -gt "$one_day_in_seconds" ]; then
+    echo "Detected that we aren't within a day of the end of the sprint, exiting.."
+    exit 1
+fi
+
 echo " -- Navigating to " $1 " --"
 cd $1
 
 git fetch origin
-
-echo " -- Stashing any uncommitted changes --"
-git stash
 
 echo " -- Resetting 'develop' branch to latest master --"
 git checkout develop
@@ -27,9 +40,6 @@ echo " -- Resetting 'master' branch to latest master --"
 git checkout master
 git reset --hard origin/master
 
-echo " -- Popping uncommitted changes --"
-git stash pop
-
-if [  ! -z $2 ]; then
+if [  ! -z $3 ]; then
     curl -X POST -H 'Content-type: application/json' --data '{"text": "Successfully reset develop and staging branches to latest master"}' $2  
 fi
